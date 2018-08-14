c70280c775a859b927e5fba6a6de1f29df122ddb
Robert Faldo <robertfaldo@gmail.com>
2018-08-14 15:34:39 +0100

    Add css to homepage (MVP complete)
    After this commit, the background color is the same
    as the FT website and all of the articles appear
    next to eachother in boxes.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express();
 const axios = require('axios');
+var path = require('path');
 
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   axios({
