3fe3ed05fa397a75db592c308a87be89ee587e3e
Robert Faldo <robertfaldo@gmail.com>
2018-08-16 12:13:00 +0100

    Refactored axios to axios.post
    In order to make it easier to stub the axios method
    I have refactored the axios call to an axios.post

--- services/ftApiCaller.js
+++ services/ftApiCaller.js
@@ -1,22 +1,20 @@
 const axios = require('axios');
 
 const ftApiQuery = function(query) {
+  const data = {
+    "queryString": query.queryString,
+    "resultContext" : {
+       "aspects" :[  "title","lifecycle","location","summary","editorial" ]
+     }
+  }
   const apiQueryResults = new Promise(function(resolve, reject) {
-    axios({
-      method: 'post',
-      url: 'http://api.ft.com/content/search/v1',
+    axios.post('http://api.ft.com/content/search/v1', data, {
       params: {'HTTP_CONTENT_LANGUAGE': 'application/json'},
       headers: {
         'X-Api-Key': '59cbaf20e3e06d3565778e7b3cf30e4ecd1d4acc8ad7c328aa6c76bb',
         'Content-Type': 'application/json'
       },
-      data: {
-        "queryString": query.queryString,
-        "resultContext" : {
-           "aspects" :[  "title","lifecycle","location","summary","editorial" ]
-         }
-      }
-      })
+    })
     .then(response => {
       const articles = response.data.results[0].results
       resolve(articles)
--- test/homepage-test.js
+++ test/homepage-test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../index');
 const expect = require('chai').expect;
 
+
 describe('Server path /', () => {
   describe('GET', () => {
     it('makes a call to FT API for all articles', async () => {
