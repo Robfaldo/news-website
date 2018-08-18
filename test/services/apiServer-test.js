c5d0669f39c9aed5ea3d8077fc3e4a9c9fae5937
Robert Faldo <robertfaldo@gmail.com>
2018-08-18 22:00:00 +0100

    Add isolated tests for apiServer
    Before this, the actual API call that was being
    made by the ApiService was not being tested, only
    that the server was calling the requestData method.
    After this, tests that are spying on the axios post
    request are added to cover the required behaviour

---
+++ test/services/apiServer-test.js
@@ -0,0 +1,35 @@
+const {assert} = require('chai');
+const sinon = require('sinon');
+const ApiService = require('../../services/apiService.js')
+const axios = require('axios');
+
+describe('ApiService', () => {
+  let spy;
+  beforeEach(() => {
+    spy = sinon.spy(axios, 'post');
+    ApiService.requestData({ queryString: 'Example query string'});
+  });
+
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe('#requestData', () => {
+    it('makes a single post request to the FT API url', () => {
+      const expectedUrl = 'http://api.ft.com/content/search/v1'
+      assert.equal(spy.calledOnce, true);
+      assert.deepEqual(spy.args[0][0], expectedUrl);
+    });
+    it('makes request with the api key and content type as headers', () => {
+      const expectedHeaders = {
+        'X-Api-Key': '59cbaf20e3e06d3565778e7b3cf30e4ecd1d4acc8ad7c328aa6c76bb',
+        'Content-Type': 'application/json'
+      }
+      assert.deepEqual(spy.args[0][2].headers, expectedHeaders);
+    });
+    it('makes request with the queryString provided', () => {
+      const expectedQuery = 'Example query string'
+      assert.deepEqual(spy.args[0][1].queryString, expectedQuery);
+    });
+  });
+});
