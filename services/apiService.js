4949aad38372637609d21d7d3953be6bbf293d16
Robert Faldo <robertfaldo@gmail.com>
2018-08-19 20:38:48 +0100

    Fixed lint errors and warnings on services/apiService.js

--- services/apiService.js
+++ services/apiService.js
@@ -1,33 +1,32 @@
 const axios = require('axios');
 
-let ApiService = {
+const ApiService = {
 
-  requestData: function(query) {
-    const url = 'http://api.ft.com/content/search/v1'
+  requestData(query) {
+    const url = 'http://api.ft.com/content/search/v1';
     const data = {
-      "queryString": query.queryString,
-      "resultContext" : {
-         "aspects" :[  "title","lifecycle","location","summary","editorial" ]
-       }
-    }
+      queryString: query.queryString,
+      resultContext: {
+        aspects: ['title', 'lifecycle', 'location', 'summary', 'editorial'],
+      },
+    };
 
-    return new Promise(function(resolve, reject) {
+    return new Promise((resolve, reject) => {
       axios.post(url, data, {
         headers: {
           'X-Api-Key': '59cbaf20e3e06d3565778e7b3cf30e4ecd1d4acc8ad7c328aa6c76bb',
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
       })
-      .then(response => {
-        const articles = response.data.results[0].results
-        resolve(articles)
-      })
-      .catch(error => {
-        console.log(error);
-      });
+        .then((response) => {
+          const articles = response.data.results[0].results;
+          resolve(articles);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     });
-
-  }
-}
+  },
+};
 
 module.exports = ApiService;
