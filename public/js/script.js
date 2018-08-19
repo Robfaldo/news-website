63b991f04cd73b83eb9d72ecdd763e9e61ccfc51
Robert Faldo <robertfaldo@gmail.com>
2018-08-19 14:12:45 +0100

    Refactor getSearchResults() by extracting method
    Before this, getSearchResults (which performs an
    Ajax request) had the logic that built the article.
    After this commit, the logic is extracted to a
    buildArticle() function which builds the article and
    is invoked by getSearchResults

--- public/js/script.js
+++ public/js/script.js
@@ -21,17 +21,21 @@ function getSearchResults() {
       success: function(data) {
         $( ".articles-container" ).empty();
         data.forEach(function(article, i){
-          $(".articles-container").append(
-            `<div class="article">
-              <div id="article-${i + 1}" class="article-title">
-                <a href=${article.location.uri}>${article.title.title}</a>
-              </div>
-              <div class="article-summary">
-                ${article.summary.excerpt}
-              </div>
-            </div>`
-          )
+          buildArticle(article, i, $searchText)
         });
       }
   });
+}
+
+function buildArticle(article, i, query) {
+  $(".articles-container").append(
+    `<div class="article">
+      <div id="article-${i + 1}" class="article-title">
+        <a href=${article.location.uri}>${article.title.title}</a>
+      </div>
+      <div class="article-summary">
+        ${article.summary.excerpt}
+      </div>
+    </div>`
+  )
 }
